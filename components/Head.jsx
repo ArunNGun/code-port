9f6f8df88672314753d485dd8b008cd88c4b3ebb
C4PT4INNULL <chaudharyarun5797@gmail.com>
2023-06-19 16:23:38 +0530

    init

--- components/Head.jsx
+++ components/Head.jsx
@@ -18,7 +18,7 @@ const CustomHead = ({ title }) => {
         content="A full-stack developer building websites that you'd like to use."
       />
       <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
-      <meta property="og:url" content="https://a-k-ivory.vercel.app/" />
+      <meta property="og:url" content="https://ak-code.vercel.app/" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
     if (localStorage.getItem("theme")) {
       document.documentElement.setAttribute(
         "data-theme",
-        localStorage.getItem("theme")
+        localStorage.getItem("theme") || "dracula"
       );
     }
   }, []);
